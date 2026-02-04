5d01073ddfeed25ae9a1297f66c4fb28cfd02227
usrhtpawar09-star <usrhtpawar09@gmail.com>
2026-02-04 09:11:31 +0530

    Update start.py

--- Rexbots/start.py
+++ Rexbots/start.py
@@ -1,5 +1,5 @@
-# Developed by: LastPerson07 × RexBots
-# Telegram: @RexBots_Official | @THEUPDATEDGUYS
+# Developed by: usrht
+# Telegram: @usrhtff01
 import os
 import asyncio
 import random
@@ -36,9 +36,9 @@ REACTIONS = [
 ]
 
 
-dev_text = "👨‍💻 Mind Behind This Bot:\n• @DmOwner\n• @akaza7902"
+dev_text = "👨‍💻 Mind Behind This Bot:\n• @DmOwner\n• @usrhtff007"
 expected_dev_hash = "b9e63b7578bdec13f3cb3162fe5f5e93dccaba3bfd5c8ddacbb90ffdcdcce402"
-channels_text = "📢 Official Channels:\n• @ReX_update\n• @THEUPDATEDGUYS\n\nStay updated for new features!"
+channels_text = "📢 Official Channels:\n• @usrhtff01\n• @usrhtff01\n\nStay updated for new features!"
 expected_channels_hash = "e19212e571bd0f6626450dd790029d392c0748c554d4b386a0c0752f4148d37d"
 
 if (
@@ -79,8 +79,8 @@ class script(object):
 """
     ABOUT_TXT = """<b>ℹ️ About This Bot</b>
 <blockquote><b>╭────[ 🧩 Technical Stack ]────⍟</b>
-<b>├⍟ 🤖 Bot Name : <a href=http://t.me/THEUPDATEDGUYS_Bot>Save Content</a></b>
-<b>├⍟ 👨‍💻 Developer : <a href=https://t.me/DmOwner>Ⓜ️ark X Rexbots</a></b>
+<b>├⍟ 🤖 Bot Name : <a href=http://t.me/usrhtff01</a></b>
+<b>├⍟ 👨‍💻 Developer : <a href=https://t.me/DmOwner>usrht</a></b>
 <b>├⍟ 📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram Async</a></b>
 <b>├⍟ 🐍 Language : <a href='https://www.python.org/'>Python 3.11+</a></b>
 <b>├⍟ 🗄 Database : <a href='https://www.mongodb.com/'>MongoDB Atlas Cluster</a></b>
